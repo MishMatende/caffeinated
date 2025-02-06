0b86a969bdf552ba09a676bf01bb5fcd6945c74f
Mishael Matende <mishaelmatende@gmail.com>
2025-02-06 17:06:55 +0300

    modal and authentication layout

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+
 export default function Authentication() {
-  return <></>;
+  const [isRegistration, setIsRegistration] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [authenticating, setAuthenticating] = useState(false);
+
+  return (
+    <>
+      <h2 className="sign-up-text">Sign Up / Login</h2>
+      <p>Sign in to your account!</p>
+      <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="*********"
+        type="password"
+      />
+      <button>
+        <p>Submit</p>
+      </button>
+      <hr />
+      <div className="register-content">
+        <p>Don&apos;t have an account?</p>
+        <button>
+          <p>Sign up</p>
+        </button>
+      </div>
+    </>
+  );
 }
