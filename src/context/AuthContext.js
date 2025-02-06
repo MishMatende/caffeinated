ed4f023509ed4c09d8c5a70684f96538da297f8d
Mishael Matende <mishaelmatende@gmail.com>
2025-02-06 19:29:40 +0300

    firebase authentication

---
+++ src/context/AuthContext.js
@@ -0,0 +1,80 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { useState, useEffect, useContext, createContext } from "react";
+import { auth, db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider(props) {
+  const { children } = props;
+  const [globalUser, setGlobalUser] = useState(null);
+  const [globalData, setGlobalData] = useState(null);
+  const [isLoading, setIsloading] = useState(false);
+
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email);
+  }
+
+  function logout() {
+    setUser(null);
+    setGlobalData(null);
+    return signOut(auth);
+  }
+
+  const value = {
+    globalUser,
+    globalData,
+    setGlobalData,
+    isLoading,
+    signup,
+    login,
+    logout,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        return;
+      }
+
+      try {
+        setIsloading(true);
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        let firebaseData = {};
+        if (docSnap.exists()) {
+          console.log("Found user data");
+          firebaseData = docSnap.data();
+        }
+        setGlobalData(firebaseData);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsloading(false);
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
