0ce04004bb6b33d194be44e1f7862ecad154f726
Mishael Matende <mishaelmatende@gmail.com>
2025-02-06 18:25:46 +0300

    sign in & sign up layout

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -4,12 +4,14 @@ export default function Authentication() {
   const [isRegistration, setIsRegistration] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [authenticating, setAuthenticating] = useState(false);
+  const [isauthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleAuthenticate() {}
 
   return (
     <>
-      <h2 className="sign-up-text">Sign Up / Login</h2>
-      <p>Sign in to your account!</p>
+      <h2 className="sign-up-text">{isRegistration ? "Sign Up" : "Login"}</h2>
+      <p>{isRegistration ? "Create an account" : "Sign in to your account!"}</p>
       <input
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -21,14 +23,18 @@ export default function Authentication() {
         placeholder="*********"
         type="password"
       />
-      <button>
+      <button onClick={handleAuthenticate}>
         <p>Submit</p>
       </button>
       <hr />
       <div className="register-content">
-        <p>Don&apos;t have an account?</p>
-        <button>
-          <p>Sign up</p>
+        <p>
+          {isRegistration
+            ? "Already have an account?"
+            : "Don't have an account?"}
+        </p>
+        <button onClick={() => setIsRegistration(!isRegistration)}>
+          <p>{isRegistration ? "Sign in" : "Sign Up"}</p>
         </button>
       </div>
     </>
